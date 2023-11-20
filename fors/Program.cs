af6364ae2de8e31dbef588368f91fdd7a2e6b7c1
Tim Dams <dams.tim@telenet.be>
2023-11-20 08:54:49 +0100

    Add project files.

---
+++ fors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace fors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //menu
+
+            //start het spel
+            bool isGameOver = false;
+            const int FRAME_TIME = 1000; //ms
+            Console.CursorVisible = false;
+            int teller = 1;
+
+            //Start gameloop
+            while (!isGameOver)
+            {
+                //1.verwerk invoer (toetsenbord)
+
+                //2.update game(state)
+
+                //3.render (naar console)
+
+                //4.wachten
+                System.Threading.Thread.Sleep(FRAME_TIME);
+                Console.Clear();
+
+            }
+
+        }
+    }
+}
